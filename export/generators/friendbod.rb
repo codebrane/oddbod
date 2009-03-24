3fd62820dd4543e14fe65b211b5cfccee7a9ec30
Alistair Young <alistair@codebrane.com>
2009-03-24 00:10:49 +0000

    Removed unused code

--- export/generators/friendbod.rb
+++ export/generators/friendbod.rb
@@ -9,11 +9,10 @@ class FriendBod < GenericBod
   def odd
     @friends.each do |friends|
       friends.each do |friend|
-#        puts "friend : #{friend.owner_username} -> #{friend.username}"
         @root_node.add_element("relationship",
                                { "uuid" => "",
                                  "uuid_one" => UUID_PERSON + friend.owner_username,
-                                 "type" => "hasfriend",
+                                 "type" => RELATIONSHIP_FRIEND_CLASS,
                                  "uuid_two" => UUID_PERSON + friend.username })
       end
     end
